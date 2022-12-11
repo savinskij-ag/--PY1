05d55c013ad185644c4355693db43b9e3e72d8ed
savinskij-ag <119214171+savinskij-ag@users.noreply.github.com>
2022-12-11 22:40:08 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -1,21 +1,12 @@
-from random import randint
+import random
+
 def get_unique_list_numbers() -> list[int]:
     unique = []
-    for number in list_unique_numbers:
-        if number in unique:
-            continue
-        else:
-            if len(unique) < 15:
-                unique.append(number)
-                list_unique_numbers[number]==list_unique_numbers[number - 1]
-            else: break
+    while len(unique) != 15:
+        number = random.randint(-10, 10)
+        if number not in unique:
+            unique.append(number)
     return unique
-
-# TODO написать функцию для получения списка уникальных целых чисел
-
-list_unique_numbers = []
-for x in range(-12, 10):
-    list_unique_numbers.append(randint(-10, 10))
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
