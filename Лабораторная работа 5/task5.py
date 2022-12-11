b411b2bb7bf71764d675a4be295c512801dd954b
savinskij-ag <119214171+savinskij-ag@users.noreply.github.com>
2022-12-11 22:41:26 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -1,16 +1,9 @@
-def get_random_password() -> str:
-    ...  # TODO написать функцию генерации случайных паролей
 import random
 
-chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-number = 25
-n = 8
-number = int(number)
-length = int(n)
-for n in range(number):
-    password =''
-    for i in range(length):
-        password += random.choice(chars)
-    print(password)
-print(get_random_password())
+chars = 'abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ0123456789'
 
+def get_random_password(n: int) -> str:
+  password = random.sample(chars, n)
+  return ''.join(password)
+
+print(get_random_password(8))
